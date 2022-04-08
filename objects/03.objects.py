a847cf00429cc4174d68102bca6190efeb8dec69
karen <karenqueca9@gmail.com>
2022-04-07 22:06:05 -0400

    my first class

--- objects/03.objects.py
+++ objects/03.objects.py
@@ -6,3 +6,26 @@ print(type(1))
 print(type(['a','b','c']))
 print(type(True)) 
 print(type("Hello World"))
+
+class Point:
+    def reset(self):
+        self.x = 0
+        self.y = 0
+    def move(self,x,y):
+        self.x = x
+        self.y = y    
+
+point1 = Point() 
+point2 = Point()
+
+
+point1.move(5,4) 
+
+point2.move(3,6) 
+
+print(type(point1))
+
+print(point1.x, point1.y)
+
+point1.reset()
+print(point1.x, point1.y)
